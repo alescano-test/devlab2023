7a34b9c2a86e136cab9edcd9798350bbe5641b76
Alejandro Lescano <alescano.test@gmail.com>
2023-06-03 20:53:36 -0300

    Resulución de ejercicio 2

---
+++ primerParcial/ejercicio2/cuadrado.js
@@ -0,0 +1,7 @@
+export default function superficie(x, y){
+    return y * x;
+}
+
+export function perímetro(lado){
+    return lado * 4;
+}
---
+++ primerParcial/ejercicio2/main.js
@@ -0,0 +1,5 @@
+import porDefecto from "./cuadrado.js";
+import { perímetro  } from "./cuadrado.js";
+
+console.log(porDefecto(4,4));
+console.log(perímetro(4));
