8d92858870ae427dd3b1572d66c23f95cf92bd6b
Alejandro Lescano <alescano.test@gmail.com>
2023-06-03 21:17:18 -0300

    Resolución ejercicio 1 de trabajo práctico 2

---
+++ trabajoPractico2/ejercicio1/main.js
@@ -0,0 +1,7 @@
+import sumar , {restar, multiplicar, dividir} from "./aritmetica.js"
+
+
+console.log(sumar(9,3));
+console.log(restar(16,3));
+console.log(multiplicar(2,7));
+console.log(dividir(32,2));
