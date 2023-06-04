0cee7693c2005009fba8e12447919628e5512a06
Miguel Alejandro Lescano <alescano.test@gmail.com>
2023-06-04 14:45:59 -0300

    Cambios en trabajo práctico 2

--- trabajoPractico2/ejercicio2y3/main.js
+++ trabajoPractico2/ejercicio2y3/main.js
@@ -2,5 +2,10 @@ import os from "node:os";
 import prettyBytes from "pretty-bytes";
 import colors from "colors"
 const free_memory = prettyBytes(os.freemem());
+const cpu_architecture = os.arch();
+const hostName = os.hostname()
+
 //! Ouput
-console.log(`Tenés: ${free_memory} de memoria RAM disponibles.`.rainbow)
+console.log(`La arquitectura de tu procesador es ${cpu_architecture}`.bgBlue);
+console.log(`Tenés: ${free_memory} de memoria RAM disponibles.`.rainbow);
+console.log(`Tenés: ${hostName} de memoria RAM disponibles.`.bgMagenta);
