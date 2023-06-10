614e036b7dac583a361e929b5f4110abe5188fa1
Alejandro Lescano <alescano.test@gmail.com>
2023-06-10 18:54:02 -0300

    reactappjs

---
+++ reactApp8-6/reactApp8-6/src/App.jsx
@@ -0,0 +1,15 @@
+function MyButton() {
+  return (
+    <button>Im a button</button>
+  );
+}
+function App() {
+  return (
+    <>
+    <h1>Welcome to my app</h1>
+    <MyButton />
+    </>
+  )
+}
+
+export default App
